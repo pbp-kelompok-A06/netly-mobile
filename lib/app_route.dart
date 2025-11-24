207e4fa4df2e73cee934f6c86f18ed554c0675d8
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-11-24 09:32:18 +0700

    add mvc arc

---
+++ lib/app_route.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// import 'modules/auth/view/login_page.dart';
+// import 'modules/home/view/home_page.dart';
+// import 'modules/profile/view/profile_page.dart';
+
+class AppRoutes {
+  static const login = '/login';
+  static const home = '/home';
+  static const profile = '/profile';
+
+  static final Map<String, WidgetBuilder> routes = {
+    login: (context) => null,
+    home: (context) => null,
+  };
+}
