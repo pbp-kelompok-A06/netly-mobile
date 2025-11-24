a6cccf4f9d220f3a18db5bed59bbacdbdc4cba17
Shotheaven <evan@Shotheavens-MacBook-Air.local>
2025-11-24 10:28:25 +0700

    sample routing

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:netly_mobile/modules/auth/view/login_page.dart';
 
-// import 'modules/auth/view/login_page.dart';
-// import 'modules/home/view/home_page.dart';
-// import 'modules/profile/view/profile_page.dart';
 
 class AppRoutes {
   static const login = '/login';
-  static const home = '/home';
-  static const profile = '/profile';
 
   static final Map<String, WidgetBuilder> routes = {
-    login: (context) => null,
-    home: (context) => null,
+    login: (context) => LoginPage(),
   };
 }
